b02a61c672b5c53548a8f85935f5963aa07ce166
storkyeh <121bbc@163.com>
2016-07-20 16:58:53 +0800

    update port

--- config/index.js
+++ config/index.js
@@ -4,7 +4,6 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
@@ -18,7 +17,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 8090,
     assetsSubDirectory: 'static',
     assetsPublicPath: '',
     proxyTable: {},
